07d94513a29c73b68cc36a4dc19b07619ec080de
Ricardo Alexis Quiroz Bazan <raquirozbazan@gmail.com>
2019-08-02 23:57:40 -0500

    feat: use api routing

---
+++ pages/api/index.js
@@ -0,0 +1,15 @@
+import crypto from 'crypto'
+
+export default (req, res) => {
+  const salt = crypto.randomBytes(16).toString('base64')
+
+  const buffer = Buffer.from(salt, 'base64')
+
+  const { text } = req.query
+
+  const hash = crypto
+    .pbkdf2Sync(text, buffer, 10000, 64, 'sha1')
+    .toString('base64')
+
+  res.json({ text, salt, hash })
+}
